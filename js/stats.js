c617ef12a97a290f87b3be00477e512f227483a2
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-06-20 19:47:47 +0200

    confere nome

--- js/stats.js
+++ js/stats.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
-        var staticUrl = 'https://api.github.com/users/juninhoojl/repos'
-        var login = false 
+        var staticUrl = 'https://api.github.com/users/juninhoojl/repos';
+        var login = true;
+        var nameConfere = "Calculadora";
         $.getJSON(staticUrl, function(data){
             var teste = 'teste';
             $(data).each(function(index, value){
                 for(i=0;i<data.length;i++){
-                    if(data[index].name == "Calculadora" && login){
+                    if(data[index].name == nameConfere && login){
                     var record="<tr><td>"+(index+1)+"</td><td>"+value.name.toUpperCase()+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
                     }else{
                     var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";     
