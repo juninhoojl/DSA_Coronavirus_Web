ab557c44a5ea07e46657af581edb8c8159498a9d
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-06-21 11:20:02 +0200

    grafico

--- js/stats.js
+++ js/stats.js
@@ -7,6 +7,78 @@ $(document).ready(function(){
         $(data).each(function(index, value){
             for(i=0;i<data.length;i++){
                 if(data[index].name == nameConfere && login){
+                    var record="<tr><td>"+(index+1)+"</td><td>"+value.name.toUpperCase()+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
+                }else{
+                    var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
+                }
+            }
+            $("table").append(record);
+        });
+    });
+
+        var a = ['a','a','a','b','b','c','e'];
+        // Contar a quantidade de cada um do unique
+        var counts = {};
+        for (var i = 0; i < a.length; i++) {
+            counts[a[i]] = 1 + (counts[a[i]] || 0);
+
+        }
+        var lItem = [];
+        var lValues = [];
+        for (var key in counts) {
+            // check if the property/key is defined in the object itself, not in parent
+            if (counts.hasOwnProperty(key)) {
+                lItem.push(key);
+                lValues.push(counts[key]);
+                //console.log(key);
+                //console.log(counts[key]);
+            }
+        }
+
+        //var unique = Array.from(new Set(a));
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+        // The data for our dataset
+        data: {
+            // O reduzido
+            labels: lItem,
+            datasets: [{
+                label: 'Level',
+                backgroundColor: '#95A0E8',
+                borderColor: '#95A0E8',
+                data: lValues
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+        scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+                stepSize: 1
+            }
+        }]
+    }
+
+        }
+    });
+
+});
+
+
+/*
+$(document).ready(function(){
+    var staticUrl = 'https://api.github.com/users/juninhoojl/repos';
+    var login = true;
+    var nameConfere = "Calculadora";
+    $.getJSON(staticUrl, function(data){
+        var teste = 'teste';
+        $(data).each(function(index, value){
+            for(i=0;i<data.length;i++){
+                if(data[index].name == nameConfere && login){
                 var record="<tr><td>"+(index+1)+"</td><td>"+value.name.toUpperCase()+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
                 }else{
                 var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
@@ -16,3 +88,4 @@ $(document).ready(function(){
         });
     });
 });
+*/
