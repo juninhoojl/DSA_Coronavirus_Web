1eaf23966a92879040591b3c2a266b8e9991627e
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-06-22 10:47:17 +0200

    graficos func

--- js/stats.js
+++ js/stats.js
@@ -1,28 +1,40 @@
 $(document).ready(function(){
-    var staticUrl = 'https://api.github.com/users/juninhoojl/repos';
-    var login = true;
+    var staticUrl = 'http://localhost:8080/dsaApp/stats/userranking';
+    var teste = false;
     var nameConfere = "Calculadora";
+    var vetNivel = new Array();
+    
     $.getJSON(staticUrl, function(data){
-        var teste = 'teste';
         $(data).each(function(index, value){
-            for(i=0;i<data.length;i++){
-                if(data[index].name == nameConfere && login){
-                    var record="<tr><td>"+(index+1)+"</td><td>"+value.name.toUpperCase()+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
-                }else{
-                    var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
-                }
-            }
+
+            //for(i=0;i<data.length;i++){
+                var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+value.level+"</td><td>"+value.exp+"</td></tr>";
+                vetNivel.push(value.exp);
+            //}
+
+            
+                // Criar vetor de nivel
+            
             $("table").append(record);
         });
+        if(!teste){
+                teste=true;
+                graficos(vetNivel);
+                console.log(vetNivel);
+            }
+
     });
 
-        var a = ['1','1','1','2','2','3','4'];
-        // Contar a quantidade de cada um do unique
-        var counts = {};
-        for (var i = 0; i < a.length; i++) {
-            counts[a[i]] = 1 + (counts[a[i]] || 0);
-        }
+});
 
+function graficos(vetNivel) {
+            //var a = ['1','1','1','2','2','3','4'];
+        // Contar a quantidade de cada um do unique
+        
+        var counts = {};
+        for (var i = 0; i < vetNivel.length; i++) {
+            counts[vetNivel[i]] = 1 + (counts[vetNivel[i]] || 0);
+        }
         var lItem = [];
         var lValues = [];
         for (var key in counts) {
@@ -35,117 +47,56 @@ $(document).ready(function(){
             }
         }
 
-        //var unique = Array.from(new Set(a));
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-        responsive: true,
-        // The data for our dataset
-        data: {
-            // O reduzido
-            labels: lItem,
-            datasets: [{
-                label: 'Level',
-                backgroundColor: '#95A0E8',
-                borderColor: '#95A0E8',
-                data: lValues
-            }]
-        },
+        MostraGraficos(lItem,lValues,"myChart");
+        MostraGraficos(lItem,lValues,"myChart2");
+}
 
-        // Configuration options go here
-        options: {
-            legend: {
-             labels: {
-                  fontColor: '#FFFFFF'
-                 }
-              },
-            title: {
-                display: false
-            },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        stepSize: 1,
-                        fontColor: '#FFFFFF'
-                    }
-                }],
-                xAxes: [{
-                    ticks: {
-                        fontColor: '#FFFFFF'
-                    }
+function MostraGraficos(label,valores,name) {
+
+            //var unique = Array.from(new Set(a));
+        var ctx = document.getElementById(name).getContext('2d');
+            var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
+            responsive: true,
+            // The data for our dataset
+            data: {
+                // O reduzido
+                labels: label,
+                datasets: [{
+                    label: 'Level',
+                    backgroundColor: '#95A0E8',
+                    borderColor: '#95A0E8',
+                    data: valores
                 }]
-            }
-        }
-    });
-
-    // Grafico 2
-var ctx = document.getElementById('myChart2').getContext('2d');
-    var chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'bar',
-        responsive: true,
-        // The data for our dataset
-        data: {
-            // O reduzido
-            labels: lItem,
-            datasets: [{
-                label: 'Level',
-                backgroundColor: '#95A0E8',
-                borderColor: '#95A0E8',
-                data: lValues
-            }]
-        },
-
-        // Configuration options go here
-        options: {
-            legend: {
-             labels: {
-                  fontColor: '#FFFFFF'
-                 }
-              },
-            title: {
-                display: false
             },
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true,
-                        stepSize: 1,
-                        fontColor: '#FFFFFF'
-                    }
-                }],
-                xAxes: [{
-                    ticks: {
-                        fontColor: '#FFFFFF'
-                    }
-                }]
-            }
-        }
-    });
 
-
-});
-
-
-/*
-$(document).ready(function(){
-    var staticUrl = 'https://api.github.com/users/juninhoojl/repos';
-    var login = true;
-    var nameConfere = "Calculadora";
-    $.getJSON(staticUrl, function(data){
-        var teste = 'teste';
-        $(data).each(function(index, value){
-            for(i=0;i<data.length;i++){
-                if(data[index].name == nameConfere && login){
-                var record="<tr><td>"+(index+1)+"</td><td>"+value.name.toUpperCase()+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
-                }else{
-                var record="<tr><td>"+(index+1)+"</td><td>"+value.name+"</td><td>"+teste+"</td><td>"+teste+"</td><td>"+teste+"</td></tr>";
+            // Configuration options go here
+            options: {
+                legend: {
+                 labels: {
+                      fontColor: '#FFFFFF'
+                     }
+                  },
+                title: {
+                    display: false
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                            fontColor: '#FFFFFF'
+                        }
+                    }],
+                    xAxes: [{
+                        ticks: {
+                            fontColor: '#FFFFFF'
+                        }
+                    }]
                 }
             }
-            $("table").append(record);
         });
-    });
-});
-*/
+
+
+}
