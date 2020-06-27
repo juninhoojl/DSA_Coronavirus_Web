4d0fdc4eeeaef3ddb7d786c79228cda0a83d1a0b
José Luiz Corrêa Junior <juninhopc@icloud.com>
2020-06-27 12:10:51 +0200

    chat quase ok

--- js/chat.js
+++ js/chat.js
@@ -1,131 +1,134 @@
+window.vComent = []; // create an empty array
 $(document).ready(function(){
 
 	// Vai pedir as coisas
-	var staticUrl = 'http://localhost:8080/dsaApp/chat';
+	window.pFun = true;
+	if(window.pFun){
+		firstFunction();
+	}
 
-    // pega o nome
-    $.getJSON(staticUrl, function(data){
-        $(data).each(function(index, value){
-        	console.log(value.id);
-        	montaCartao(value.content,value.username,value.receivedDate, value.id);
-        });
-    });
+	// pega o nome
 });
 
-function montaCartao(conteudo, nome, sdata, id){
 
-	var reqid = 'http://localhost:8080/dsaApp/user/'+nome;
-    // Se foi completo solicitar o id
-    $.getJSON(reqid, function(data){
-
-    	var composicao = '<div class="card mb-4"><div class="card-header"><div class="media flex-wrap w-100 align-items-center"> <img src="';
-		var staticUrl = 'http://localhost:8080/dsaApp/user/image/'+data.id;
-		console.log(staticUrl);
-		$.get(staticUrl)
-	    .done(function() { 
-	    	composicao+= staticUrl;
-			composicao+='" class="d-block ui-w-40 rounded-circle" alt=""><div class="media-body ml-3"> <a href="javascript:void(0)" data-abc="true">';
-			composicao+= nome.toUpperCase();;
-			composicao+= '</a><div class="text-muted small">';
-			composicao+= data.email;
-			composicao+= '</div></div><div class="text-muted small ml-3"><div>Exp <strong>';
-			composicao+= data.exp;
-			composicao+= '</strong></div><div><strong>';
-			composicao+= sdata.substr(0, 9);
-			composicao+= '</strong></div></div></div></div><div class="card-body"><p>';
-			composicao+= conteudo;
-		    composicao+= '</p></div></div>';
-		    addCode(composicao);
-	    }).fail(function() { 
-	    	composicao+= "img/sem_imagem.jpg";
-			composicao+='" class="d-block ui-w-40 rounded-circle" alt=""><div class="media-body ml-3"> <a href="javascript:void(0)" data-abc="true">';
-			composicao+= nome.toUpperCase();;
-			composicao+= '</a><div class="text-muted small">';
-			composicao+= data.email;
-			composicao+= '</div></div><div class="text-muted small ml-3"><div>Exp <strong>';
-			composicao+= data.exp;
-			composicao+= '</strong></div><div><strong>';
-			composicao+= sdata.substr(0, 9);
-			composicao+= '</strong></div></div></div></div><div class="card-body"><p>';
-			composicao+= conteudo;
-		    composicao+= '</p></div></div>';
-		    addCode(composicao);
-	    })
-    });
+function firstFunction(data){
+	var staticUrl = 'http://localhost:8080/dsaApp/chat';
+	nomeCookie = getCookie('cookieName');
+	console.log("PRIMEIRA OK");
+	$.getJSON(staticUrl, function(data){
+	// do some asynchronous work
+	// and when the asynchronous stuff is complete
+		$(data).each(function(index, value){
+			if (nomeCookie == value.username) {
+				//var teste = montaCartao(value.content,value.username,value.receivedDate, value.id,"justify-content-end");
+				classe = "justify-content-end";
+			}else{
+				//var teste = montaCartao(value.content,value.username,value.receivedDate, value.id,"justify-content-start");
+				classe = "justify-content-start"
+			}
+			window.vComent.push({
+				sconteudo: value.content,
+				suser: value.username,
+				sdata: value.receivedDate.substr(0, 9),
+				sid: value.id,
+				sexp: "",
+				simg: "",
+				semail: "",
+				sclasse: classe
+			});
+		});
+		secondFunction();
+	});
 }
 
-function reqUrl(name){
-    var reqid = 'http://localhost:8080/dsaApp/user/'+name;
-    // Se foi completo solicitar o id
-    $.getJSON(reqid, function(data){
-		var staticUrl = 'http://localhost:8080/dsaApp/user/image/'+data.id;
-		console.log(staticUrl);
-		return staticUrl;
-    });
-}
+		function secondFunction(){
+			console.log("SEGUNDA OK");
+			// call first function and pass in a callback function which
+			// first function runs when it has completed
+					window.vComent.forEach(function(part, index, theArray){
+						var reqid = 'http://localhost:8080/dsaApp/user/'+theArray[index].suser;
+						// Se foi completo solicitar o id
+							$.getJSON(reqid, function(data){
+								var staticUrl = 'http://localhost:8080/dsaApp/user/image/'+data.id;
+								//console.log(staticUrl);
+								$.get(staticUrl)
+								.done(function() { 
+									theArray[index].simg = staticUrl;
+									theArray[index].semail = data.email;
+									theArray[index].sexp = data.exp;
+									theArray[index].suser = theArray[index].suser.toUpperCase();
+									theArray[index].sdata = theArray[index].sdata.substr(0, 9);
+									if(index == vComent.length-1){
+										thirdFunction(theArray);
+									}
+								}).fail(function() { 
+									theArray[index].simg = "img/sem_imagem.jpg";
+									theArray[index].semail = data.email;
+									theArray[index].sexp = data.exp;
+									theArray[index].suser = theArray[index].suser.toUpperCase();
+									theArray[index].sdata = theArray[index].sdata.substr(0, 9);
+									if(index == vComent.length-1){
+										thirdFunction(theArray);
+									}
+								})
+							});
+						});
+		}
 
-function reqImage(name){
+		function thirdFunction(theArray3){
+			console.log("TERCEIRA OK");
 
-    var reqid = 'http://localhost:8080/dsaApp/user/'+name;
-    console.log(reqid);
-    // Se foi completo solicitar o id
-    $.getJSON(reqid, function(data){
-    	var id = data.id;
-    	console.log(id);
-		var staticUrl = 'http://localhost:8080/dsaApp/user/image/'+id;
-	    // pega o nome
-	    $.getJSON(staticUrl, function(data){
-	        return console.log(value);
-	    });
-    });
-}
-
-function addCode(cartao) { 
-	document.getElementById("DIVCARTAO").insertAdjacentHTML("beforeend", 
-		cartao); 
-}
-
+			theArray3.forEach(function(item, index, theArray){
+					console.log(theArray[index]);
+					var composicao = '<div class="row ';
+					composicao+=theArray[index].sclasse;
+					composicao+=' conjuntoChatt"><div class="col-md-10 col-lg-8 col-sm-12 float-right"> <div class="card mb-4';
+					composicao+=' "><div class="card-header"><div class="media flex-wrap w-100 align-items-center"> <img src="';
+					composicao+= theArray[index].simg;
+					composicao+='" class="d-block ui-w-40 rounded-circle" alt=""><div class="media-body ml-3"> <a href="javascript:void(0)" data-abc="true">';
+					composicao+= theArray[index].suser;
+					composicao+= '</a><div class="text-muted small">';
+					composicao+= theArray[index].semail;
+					composicao+= '</div></div><div class="text-muted small ml-3"><div>Exp <strong>';
+					composicao+= theArray[index].sexp;
+					composicao+= '</strong></div><div><strong>';
+					composicao+= theArray[index].sdata;
+					composicao+= '</strong></div></div></div></div><div class="card-body"><p>';
+					composicao+= theArray[index].sconteudo;
+					composicao+= '</p></div></div></div></div>';
+					document.getElementById("DIVCARTAO").insertAdjacentHTML("beforeend", composicao);
+					window.scrollTo(0,document.body.scrollHeight);
+			});
+		}
 
 $(function(){
+	$('#botaoPost').on('click', function(e){
+	e.preventDefault();
 
-  $('#botaoLogin').on('click', function(e){
-    e.preventDefault();
+	// Vai conferir se esta vazio ou nao
 
-    // Vai conferir se esta vazio ou nao
-
-    var inputUser = document.getElementById("textUser").value;
-	var inputPass = document.getElementById("textPass").value;
-
-	var Credentials = {
-		"name": inputUser,
-		"password": inputPass
+	var ReceivedMessage = {
+		"username": getCookie('cookieName'),
+		"content": document.getElementById("textPost").value
 	};
-
+	
+	console.log(ReceivedMessage);
+	
+	//curl -X POST "http://localhost:8080/dsaApp/chat" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"username\": \"juninho\", \"content\": \"Mensagem 2...\"}"
 	$.ajax({
-	    type: "POST",
-	    url: "http://localhost:8080/dsaApp/user/login",
-	    // The key needs to match your method's input parameter (case-sensitive).
-	    data: JSON.stringify(Credentials),
-	    contentType: "application/json; charset=utf-8",
-	    dataType: "json",
-	    success: function(data) {
-	    	console.log("Sucesso");
-	        console.log(data.status);
-	    },
-	    complete: function(data) {
-	    	console.log("Completo");
-	        console.log(data.status);
-	        setCookie('cookieToken',data.responseText,7);
-	        setCookie('cookieName',Credentials.name,7);
-	        console.log(data.responseText);
-	        var reqid = "http://localhost:8080/dsaApp/user/"+Credentials.name;
-	        // Se foi completo solicitar o id
-		    $.getJSON(reqid, function(data){
-		    	setCookie('cookieId',data.id,7);
-		    	console.log(data.id);
-		    	location.href = 'estatisticas.html';
-		    });
-	    }
+		type: "POST",
+		url: "http://localhost:8080/dsaApp/chat",
+		// The key needs to match your method's input parameter (case-sensitive).
+		data: JSON.stringify(ReceivedMessage),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success: function(data) {
+			console.log("Sucesso");
+		},
+		complete: function(data) {
+			location.reload();
+			console.log("Completo");
+		}
 	});
 
   }); // end click event handler
